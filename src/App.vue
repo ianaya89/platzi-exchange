be47943e51812a2f2787b9be7619860a934b71c0
Ignacio Anaya <ignacio.anaya89@gmail.com>
2019-11-12 15:35:47 -0300

    Add tailwind setup 💅

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-blue-400">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
---
+++ tailwind.js
@@ -0,0 +1,24 @@
+module.exports = {
+  important: true,
+  theme: {
+    container: {
+      center: true
+    },
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff'
+      },
+      margin: {
+        '96': '24rem',
+        '128': '32rem'
+      }
+    }
+  },
+  variants: {
+    opacity: ['responsive', 'hover']
+  }
+}
